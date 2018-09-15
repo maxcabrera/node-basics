c3adb71de66ca47a630d610593acc8388354340b
Max Cabrera <cabrera.max@gmail.com>
2018-09-14 20:32:07 -0500

    added some patterns for module.exports

--- modules.js
+++ modules.js
@@ -3,3 +3,22 @@ const billingual = require('./greetings')
 
 billingual.english()
 billingual.spanish()
+
+/**
+ * Patterns to export stuff
+ */
+/**
+    const greet = function() {
+      console.log('hello')
+    }
+
+    module.exports = greet;
+
+    module.exports.greet = greet
+
+    const Person = function() {
+      this.name = "Hacker"
+    }
+
+    module.exports = new Person();
+ **/
